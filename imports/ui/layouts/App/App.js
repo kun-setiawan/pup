c992b471179907dd06165fee72062bb10df03357
kun <kunsetiawan@byporte>
2018-03-23 15:10:10 +0700

    javascript fix

--- imports/ui/layouts/App/App.js
+++ imports/ui/layouts/App/App.js
@@ -64,21 +64,23 @@ class App extends React.Component {
     const libPath = '';
     // const libPath = 'http://ec2-54-173-3-232.compute-1.amazonaws.com:3000';
 
-    loadStyle(`${libPath}/bower_components/bootstrap/dist/css/bootstrap.min.css`);
-    loadStyle(`${libPath}/bower_components/font-awesome/css/font-awesome.min.css`);
-    loadStyle(`${libPath}/bower_components/Ionicons/css/ionicons.min.css`);
-    loadStyle(`${libPath}/dist/css/AdminLTE.min.css`);
-    loadStyle(`${libPath}/dist/css/skins/_all-skins.min.css`);
-    loadStyle(`${libPath}/bower_components/morris.js/morris.css`);
-    loadStyle(`${libPath}/bower_components/jvectormap/jquery-jvectormap.css`);
-    loadStyle(`${libPath}/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css`);
-    loadStyle(`${libPath}/bower_components/bootstrap-daterangepicker/daterangepicker.css`);
-    loadStyle(`${libPath}/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css`);
-    loadScript('https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js');
-    loadScript('https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js');
-    loadStyle('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic');
-    loadScript(`${libPath}/bower_components/jquery/dist/jquery.min.js`);
-    loadScript(`${libPath}/dist/js/adminlte.min.js`);
+    if (!props.loading) {
+      loadStyle(`${libPath}/bower_components/bootstrap/dist/css/bootstrap.min.css`);
+      loadStyle(`${libPath}/bower_components/font-awesome/css/font-awesome.min.css`);
+      loadStyle(`${libPath}/bower_components/Ionicons/css/ionicons.min.css`);
+      loadStyle(`${libPath}/dist/css/AdminLTE.min.css`);
+      loadStyle(`${libPath}/dist/css/skins/_all-skins.min.css`);
+      loadStyle(`${libPath}/bower_components/morris.js/morris.css`);
+      loadStyle(`${libPath}/bower_components/jvectormap/jquery-jvectormap.css`);
+      loadStyle(`${libPath}/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css`);
+      loadStyle(`${libPath}/bower_components/bootstrap-daterangepicker/daterangepicker.css`);
+      loadStyle(`${libPath}/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css`);
+      loadScript('https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js');
+      loadScript('https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js');
+      loadStyle('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic');
+      loadScript(`${libPath}/bower_components/jquery/dist/jquery.min.js`);
+      loadScript(`${libPath}/dist/js/adminlte.min.js`);
+    }
 
     return (
       <Router>
@@ -96,7 +98,7 @@ class App extends React.Component {
             { /* <Grid> */}
             <div className={classNames({ 'layout-top-nav': !props.authenticated })} >
               <div className="content-wrapper">
-                {!props.authenticated ? <BreadcrumbSimple /> : '' }
+                {props.authenticated ? <BreadcrumbSimple /> : '' }
                 <Switch>
                   <Route exact name="index" path="/" component={Index} />
                   <Authenticated exact path="/documents" component={Documents} setAfterLoginPath={setAfterLoginPath} {...props} {...state} />
