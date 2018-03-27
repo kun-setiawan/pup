0d9c0b54ca74c5b72dc2113da1783f8e4bc9de7c
kun <kunsetiawan@byporte>
2018-03-27 10:24:23 +0700

    jsonify: block

--- imports/ui/components/Block/BlockDescription.js
+++ imports/ui/components/Block/BlockDescription.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const BlockDescription = () => (
+const BlockDescription = props => (
   <div className="description-block border-right">
-    <span className="description-percentage text-green"><i className="fa fa-caret-up" /> 17%</span>
-    <h5 className="description-header">$35,210.43</h5>
-    <span className="description-text">TOTAL REVENUE</span>
+    <span className={classNames(`description-percentage text-${props.color}`)}><i className={classNames(`fa fa-caret-${props.icon}`)} /> {props.titleValue}%</span>
+    <h5 className="description-header">${props.subValue}</h5>
+    <span className="description-text">{props.desc}</span>
     { /* /.description-block --> */ }
   </div>
 );
 
 BlockDescription.defaultProps = {
+  color: 'green',
+  icon: 'up',
+  titleValue: '',
+  subValue: 0,
+  desc: '',
 };
 
 BlockDescription.propTypes = {
+  color: PropTypes.string,
+  icon: PropTypes.string,
+  titleValue: PropTypes.string,
+  subValue: PropTypes.number,
+  desc: PropTypes.string,
 };
 
 export default BlockDescription;
